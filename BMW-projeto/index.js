581536051912e286e9ab33b7d44b4e272d79726d
Flavio Vieira <flavio.vieiraev@gmail.com>
2024-05-28 04:23:02 -0300

    first commit

---
+++ BMW-projeto/index.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+	<title>Validação</title>
+	<script src="validacao.js"></script>
+</head>
+<body>
+	<form name="formulario">
+		<label for="nome">Nome</label>
+		<input type="text" id = "nome" name="nome" />
+		<button onclick="validaCaracter();">Enviar</button>
+	</form>
+</body>
+</html>
